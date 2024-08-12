2d1b4b71d171b8bcf28866cb3f6de7022d97c6c0
Tamara Allos <tamaraallos27@gmail.com>
2024-08-12 20:31:20 +1000

    refactor: Made minor adjustments to the email script so it follows a naming convention using a for loop

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ For Bad Security Inc (BSI) - Proof of Concept
 3. Search through .log file with a any string
 4. Generate script for creating .eml emails
 
-
+# day 2 - Tamara Allos
+1. Updated the email generator. This is so it follows a consistent naming convention (emailN - n being number of emails already created)
+ 
--- createEML.py
+++ createEML.py
@@ -1,4 +1,3 @@
-import uuid
 import os
 from email import generator
 from email.mime.multipart import MIMEMultipart
@@ -8,11 +7,11 @@ from email.mime.text import MIMEText
 # uses MIME - multiplepurpose Internet Mail Extnesion - support transfer of text and text attatchments
 
 # variables: create test email content - follow this structure please
-to_address = "daniel@email.com"
-from_address = "somehow@email.com"
-subject = "just email"
-date = "Thursday, 10 Jul 2024 18:16:34 -0000"
-body = "just another email testing words, stuff, hi no yes, bye. afternoon."
+to_address = "John.Doe@gmail.com" 
+from_address = "Jane.Doe@gmail.com"
+subject = "Admin Meeting"
+date = "Thursday, 10 Jul 2024 12:39:34 -0000"
+body = "Hey John, how is it going? I just wanted to send you a message to see if you got a message from admin? They asked me for a meeting. I was wondering if this was to do with the software we were working on? Thanks Jane."
 
 # pass in email content into parameters to create an email
 def generate_email(to_address, from_address, subject, date, body):
@@ -31,14 +30,28 @@ email = generate_email(to_address, from_address, subject, date, body)
 
 # save email to my file
 dire = "C:\\Users\\Tamara\\Desktop\\BSI\\gateway\\emails"
-file_name = str(uuid.uuid4()) + ".eml" # using .eml format for parser
+
+
+# format email file name to be 'emailN' n being number of email generated
+
+existing_file = []
+
+# loop through all the files in the dire
+for email_file in os.listdir(dire):
+    if email_file.startswith('email') and email_file.endswith('.eml'): # check if file name starts with email and ends with .eml
+        existing_file.append(email_file) # add to the list
+
+# number for file name
+next_num = len(existing_file) + 1
+
+# file name format 
+file_name = 'email' + str(next_num) + '.eml'
 
 # change working dire to the dire outlined
 os.chdir(dire)
-
 
 # write the EML format file
 with open(file_name, 'w') as file: # w opens file for writing
     eml_generator = generator.Generator(file) # convert into flat text format
     eml_generator.flatten(email)
-    print(f'email successfully saved')
+    print(f'Email successfuly created!')
