3e437d4b2e5ccd482805f1af9c409e63caa91659
hiiimken <54103791+hiiimken@users.noreply.github.com>
2024-08-28 09:27:03 +1000

    Spam and Phishing modules v2
    - Restored extract_domain function
    - Fixed comments within both programs
    - Removed keyword "free" within the spam_keywords text file, as this keywords brings false positives.
    - Changed implementation of is_spam on spamtest.py. Instead of iterating through the entire message, implemented it so that it instantly returns the result if it detects a keyword.
    - Added error checking with the load_phishing_domains and load_phishing_links function
    - Removed unused imports from both files
    - Moved files into two separate folders based on usage: spam filter and phishing filter.

---
+++ Modules/Spam Filter/spamtest.py
@@ -0,0 +1,22 @@
+from typing import List
+
+# Function to load spam keywords from a file. Returns a list of spam keywords.
+def load_spam_keywords(file_path: str) -> List[str]:
+    try:
+        with open(file_path, 'r') as file:
+            keywords = file.read().splitlines()
+        return [keyword.lower() for keyword in keywords]
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+        return []
+    except Exception as e:
+        print(f"An error occurred while reading the file {file_path}: {e}")
+        return []
+
+# Function to detect if an email is spam based on the list of spam keywords. Returns bool.
+def is_spam(email_text: str, spam_keywords: List[str]) -> bool:
+    # converts text to lowercase.
+    email_text = email_text.lower()
+
+    # Check for presence of any spam keyword in the email text
+    return any(keyword in email_text for keyword in spam_keywords)
