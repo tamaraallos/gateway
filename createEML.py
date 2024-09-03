e8afd496ca8948db582ff7ca2aa4f0baaa04216f
Tamara Allos <tamaraallos27@gmail.com>
2024-09-03 16:53:42 +1000

    refactor: Updated createEML script to include email type

--- createEML.py
+++ createEML.py
@@ -1,4 +1,3 @@
-from cryptography.fernet import Fernet
 import os
 from email import generator
 from email.mime.multipart import MIMEMultipart
@@ -9,23 +8,47 @@ load_dotenv(".env")
 
 # this script generates an email in .eml format (this is for testing purposes)
 # uses MIME - multiplepurpose Internet Mail Extnesion - support transfer of text and text attatchments
-
-# generate encryption key
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
-
 # variables: create test email content - follow this structure please
-to_address = "dassdadassd@gmail.com" 
-from_address = "meadsdsasd@gmail.com"
-subject = "test"
-date = "Monday, 12 Jul 2024 12:39:34 -0000"
-body = "test"
+to_address = "tester@gmail.com" 
+from_address = "adminBSI@gmail.com"
+subject = "Personal Info - For payslips"
+date = "Tuesday, 03 Sep 2024 12:39:34 -0000"
+#body = "test"
 action_status = "pending" 
+email_type = "normal"
+
+# for phishing test
+# body = """
+# Dear User at BSI,
+
+# Your account has been temporarily suspended due to suspicious activity. To reactivate your account, please verify your identity by clicking the link below:
+
+# https://2020logs.duckdns.org/verify
+
+# Failure to do so within 24 hours will result in permanent account deactivation.
+
+# Thank you,
+# Support Team from BSI
+# """
+
+body = """
+ Hey,
+ Hope you are well. I was just sending you the details you requested so you guys can verify my account information:
+ Credit Card Number: 4111 1111 1111 1111
+ Bank Account Number: 98765432101234567
+ PIN Number: 654321
+ Tax File Number: 123 456 789
+
+ Thanks,
+ Teddy
+"""
+
+body = body.replace("\n", " ").strip()
 
 # encrypt_body = cipher_suite.encrypt(body.encode())
 
 # pass in email content into parameters to create an email
-def generate_email(to_address, from_address, subject, date, body, action_status):
+def generate_email(to_address, from_address, subject, date, body, action_status, email_type):
     # constructs email message
     msg = MIMEMultipart() # create new message object
     # sets email headers
@@ -33,12 +56,14 @@ def generate_email(to_address, from_address, subject, date, body, action_status)
     msg["From"] = from_address
     msg["Subject"] = subject
     msg["Date"] = date
+    msg['X-Action-Status'] = action_status # custom header for the action status
+    msg['X-Type'] = email_type # custom header for the emial type
+    
     msg.attach(MIMEText(body, 'plain')) # MIME represents textual data (attatched plain text body to email)
-    msg['X-Action-Status'] = action_status
     return msg
 
 # generate the email
-email = generate_email(to_address, from_address, subject, date, body, action_status)
+email = generate_email(to_address, from_address, subject, date, body, action_status, email_type)
 
 # save email to my 'email' folder
 dire = os.getenv("EMAIL_FOLDER")
