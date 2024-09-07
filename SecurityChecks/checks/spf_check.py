60aa108994490cd2cd76c4d038041f40b0bfa280
Tamara Allos <tamaraallos27@gmail.com>
2024-09-08 01:19:57 +1000

    refactor: Updated function to take in only sender email

--- SecurityChecks/checks/spf_check.py
+++ SecurityChecks/checks/spf_check.py
@@ -10,25 +10,16 @@ def get_ip_from_domain(domain):
     ip_address = socket.gethostbyname(domain)
     return ip_address
 
-def spf_check(domain, sender):
+def spf_check(sender):
+    domain = sender.split('@', 1)[1]
     ip_address = get_ip_from_domain(domain)
     _, status_code, _ = spf.check(ip_address, domain, sender)
     return status_code
 
-# if spf status code is not 250, it fails
-def check_spf_status_code(domain, sender):
-    status_code = spf_check(domain, sender)
-    if status_code == 250:
+# if spf status code is not 250 or 451, it fails
+def check_spf_status_code(sender):
+    status_code = spf_check(sender)
+    if status_code == 250 or status_code == 451:
         return True
     else:
         return False 
-
-
-# BELOW IS AN EXAMPLE FOR TESTING PURPOSES
-# IT NEEDS TO BE DELETED AFTER TESTING.
-domain = 'gmail.com'
-sender_email = 'user@gmail.com'
-
-status_code = check_spf_status_code(domain, sender_email)
-
-print(f"spf status code: {status_code}")
