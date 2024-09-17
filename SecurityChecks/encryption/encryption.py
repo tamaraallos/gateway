dc621bb179edf2e6f57716836193d7acc72e89fc
tamaraallos <135336290+tamaraallos@users.noreply.github.com>
2024-09-17 18:50:29 +1000

    Update encryption.py

--- SecurityChecks/encryption/encryption.py
+++ SecurityChecks/encryption/encryption.py
@@ -1,10 +1,10 @@
 from cryptography.fernet import Fernet
 
-# Example encryption key (should be securely managed in a real application)
+# example encryption key (should be securely managed in a real application)
 encryption_key = Fernet.generate_key()
 cipher_suite = Fernet(encryption_key)
 
-# Encrypt email body
+# encrypt email body
 def encrypt_body(content):
     encrypted_content = cipher_suite.encrypt(content.encode('utf-8'))
     return encrypted_content.decode('utf-8')
